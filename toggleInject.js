35b9c269391e3032f982d6460ae4ab483a44effc
Henry Brown <henry.jy.brown@gmail.com>
2025-12-22 15:28:48 +0000

    checking if toggle container is found before adding all the listeners

--- toggleInject.js
+++ toggleInject.js
@@ -1,44 +1,45 @@
 const STORAGE_KEY = "geminiEnabled";
 
-document
-    .querySelector(".toggle-container")
-    .addEventListener("click", toggleResults);
-
+const toggleContainer = document.querySelector(".toggle-container");
 const toggle = document.querySelector(".toggle");
 
-function toggleResults() {
-    const checked = window.sessionStorage.getItem(STORAGE_KEY);
+// only run on pages where these divs are succesfully inserted
+if (toggle && toggleContainer) {
+    function toggleResults() {
+        const checked = window.sessionStorage.getItem(STORAGE_KEY);
+        window.postMessage(
+            {
+                source: "gemini-toggle",
+                fromPage: true,
+                newState: !(checked === "true"),
+            },
+            "*"
+        );
+    }
+    toggleContainer.addEventListener("click", toggleResults);
+
+    window.addEventListener("message", (e) => {
+        if (e.source !== window) return;
+        if (!e.data || e.data.source !== "gemini-toggle" || !e.data.fromContent)
+            return;
+
+        const toggle = document.querySelector(".toggle");
+        const geminiDiv = document.querySelector("#eKIzJc");
+        window.sessionStorage.setItem(STORAGE_KEY, e.data.geminiEnabled);
+
+        if ((toggle.checked = e.data.geminiEnabled)) {
+            geminiDiv.classList.add("reveal");
+        } else {
+            geminiDiv.classList.remove("reveal");
+        }
+    });
+
+    // check the toggle state on load (works because toggleContent runs on document idle)
     window.postMessage(
         {
             source: "gemini-toggle",
             fromPage: true,
-            newState: !(checked === "true"),
         },
         "*"
     );
 }
-
-window.addEventListener("message", (e) => {
-    if (e.source !== window) return;
-    if (!e.data || e.data.source !== "gemini-toggle" || !e.data.fromContent)
-        return;
-
-    const toggle = document.querySelector(".toggle");
-    const geminiDiv = document.querySelector("#eKIzJc");
-    window.sessionStorage.setItem(STORAGE_KEY, e.data.geminiEnabled);
-
-    if ((toggle.checked = e.data.geminiEnabled)) {
-        geminiDiv.classList.add("reveal");
-    } else {
-        geminiDiv.classList.remove("reveal");
-    }
-});
-
-// check the toggle state on load (works because toggleContent runs on document idle)
-window.postMessage(
-    {
-        source: "gemini-toggle",
-        fromPage: true,
-    },
-    "*"
-);
